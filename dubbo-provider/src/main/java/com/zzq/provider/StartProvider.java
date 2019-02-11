7df4deefa96406784fdb2b10b0071938592c42c3
zhouzhongqing <1030907690@qq.com>
2019-02-11 17:36:18 +0800

    spring

---
+++ dubbo-provider/src/main/java/com/zzq/provider/StartProvider.java
@@ -0,0 +1,16 @@
+package com.zzq.provider;
+
+/**
+ * @author Zhou Zhong Qing
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: 提供者启动
+ * @date 2019/2/11 17:35
+ */
+public class StartProvider {
+
+
+    public static void main(String[] args) {
+
+    }
+}
