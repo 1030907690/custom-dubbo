2e19e29930bf7774299eb7a4fdfb8c8b927ae1ac
zhouzhongqing <1030907690@qq.com>
2019-03-05 19:49:32 +0800

    AppConfig

---
+++ dubbo-consumer/src/main/java/com/zzq/consumer/scan/MapperScannerConfigurer.java
@@ -0,0 +1,11 @@
+package com.zzq.consumer.scan;
+
+/**
+ * @author Zhou Zhong Qing
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2019/3/519:49
+ */
+public class MapperScannerConfigurer {
+}
