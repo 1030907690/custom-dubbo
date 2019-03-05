5ad5bc5ce3783f60967747794aaf121830b2653e
zhouzhongqing <1030907690@qq.com>
2019-03-05 19:42:46 +0800

    AppConfig

--- dubbo-consumer/src/main/java/com/zzq/consumer/StartConsumer.java
+++ dubbo-consumer/src/main/java/com/zzq/consumer/StartConsumer.java
@@ -3,10 +3,8 @@ package com.zzq.consumer;
 import com.zzq.consumer.customtag.pojo.Dubbo;
 import com.zzq.consumer.registry.IServiceDiscovery;
 import com.zzq.consumer.registry.ServiceDiscoveryImpl;
-import com.zzq.consumer.scan.ClassPathMapperScanner;
-import com.zzq.consumer.scan.CustomBeanFactoryPostProcessor;
 import com.zzq.provider.api.ITestService;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -18,9 +16,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class StartConsumer {
 
-
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-base.xml");
+     /*   AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+
+*/
+       /* ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-base.xml");
 
         String[] beanNames = context.getBeanDefinitionNames();
 
@@ -33,6 +34,21 @@ public class StartConsumer {
         IServiceDiscovery serviceDiscovery = new ServiceDiscoveryImpl(dubbo.getAddress());
         RpcClientProxy rpcClientProxy = new RpcClientProxy(serviceDiscovery);
         ITestService testService = rpcClientProxy.create(ITestService.class);
+        //System.out.println("testService "+ testService);
+        String obj = testService.test("hello ");
+        System.out.println(obj);*/
+
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-base.xml");
+
+        String[] beanNames = context.getBeanDefinitionNames();
+
+        for (String beanName :  beanNames) {
+            System.out.println("beanName: "+ beanName);
+        }
+
+
+        ITestService testService = context.getBean(ITestService.class);
         //System.out.println("testService "+ testService);
         String obj = testService.test("hello ");
         System.out.println(obj);
