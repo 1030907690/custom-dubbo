c82cc2bcf4a800330f6c9ad68c079ebdbdcf3a0a
zhouzhongqing <1030907690@qq.com>
2019-07-10 11:46:45 +0800

    jdk

--- dubbo-consumer/src/main/java/com/zzq/consumer/RpcClientProxy.java
+++ dubbo-consumer/src/main/java/com/zzq/consumer/RpcClientProxy.java
@@ -18,6 +18,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/**
+ * jdk的代理
+ * */
 public class RpcClientProxy {
 
     private IServiceDiscovery serviceDiscovery;
